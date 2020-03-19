95054205bed5b58342a7ba01fd3549b36049ae82
Jorge Cruz <jlcruz7125@gmail.com>
2020-03-19 16:31:33 -0400

    Test 02 Done

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,55 @@
 #write your code here
+def add(first, second)
+    first + second
+end
+
+def subtract(first, second)
+    first - second
+end
+
+def sum(arr)
+
+    total = 0;
+    arr.length.times do |i|
+        total += arr[i]
+    end
+
+    total
+
+end
+
+def multiply(arr)
+
+    total = 1;
+    arr.length.times do |i|
+        total *= arr[i]
+    end
+
+    total
+end
+
+def power(base, pow)
+
+    result = 1
+    pow.times do
+        result *= base
+    end
+
+    result
+
+end
+
+def factorial(num)
+
+    if(num == 0 or num == 1)
+        return 1
+    elsif
+        res = 1
+        while num > 1
+            res *= num
+            num -= 1
+        end
+
+        return res
+    end
+end
