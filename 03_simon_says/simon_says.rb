7c17e9445d54cb763552e2c0a8184562108b748c
Jorge Cruz <jlcruz7125@gmail.com>
2020-03-19 19:11:31 -0400

    Test 03 Done

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,44 @@
 #write your code here
+def echo(msg)
+    msg
+end
+
+def shout(msg)
+    msg.upcase
+end
+
+def repeat(msg, num=2)
+    res = msg
+    (num - 1).times do
+        res += " #{msg}"
+    end
+    return res
+end
+
+def start_of_word(word, len)
+    word[0..len-1]
+end
+
+def first_word(words)
+    word = words.split
+    return word[0]
+end
+
+def titleize(message)
+    little_words = ["and", "or", "the", "over", "to", "the", "a", "but", "by", "of", "from", "is", "on"]
+    array = message.split
+
+    res = ""
+
+    array.length.times do |i|
+        if(i == 0)
+            res += array[i].capitalize
+        elsif(little_words.include? array[i])
+            res += " #{array[i]}"
+        else
+            res += " #{array[i].capitalize}"
+        end
+    end
+
+    res.strip
+end
