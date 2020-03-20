9fcc963fc47f55242fa4ddfbe67953ef34ae232b
Jorge Cruz <jlcruz7125@gmail.com>
2020-03-19 21:35:53 -0400

    Test 05 Done

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,33 @@
 class Book
 # write your code here
+
+    # Getter
+    attr_reader :title
+
+    def initialize()
+    end
+
+    # Setter
+    def title=(name)
+        little_words = ["and", "or", "the", "over", "to", "the", "a", "but", "by", "of", "from", "is", "on", "an", "in"]
+        array = name.split
+
+        result = ""
+        array.length.times do |i|
+            if (i == 0)
+                result += "#{array[i].capitalize} "
+            elsif (array[i] == "i")
+                result += "I "
+            elsif (little_words.include? array[i])
+                result += "#{array[i]} "
+            else
+                result += "#{array[i].capitalize} "
+            end
+        end
+
+        @title = result.strip
+
+    end
+
+
 end
